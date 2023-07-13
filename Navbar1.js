544f2ad23d085918617ff4035dadc6337ac92b81
Divyadharshini999 <dharshini999dd@gmail.com>
2023-07-13 16:29:45 +0530

    last3cmparechanges

--- Navbar1.js
+++ Navbar1.js
@@ -1,9 +1,11 @@
 //Navbar1
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link ,React,BrowserRouter} from "react-router-dom";
 const Navbar1 =() =>{
     return(
-       <div className="hellowelcome"></div>
+       <div className="hellowelcome">kjgkg yfyfiy</div>
+       <div className="hellowel">kjgkg yfyfiy</div>
+
     )
 console.log(Navbar1)
 }
